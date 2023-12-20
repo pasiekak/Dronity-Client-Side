a6e53be007e27796cea8b6ee29f8080e56d98bc3
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2023-12-20 11:48:43 +0100

    Created custom hook for scrolling page

---
+++ src/shared/hooks/useBodyScrollable.jsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+
+const useBodyScrollable = () => {
+    const [bodyScrollable, setBodyScrollable] = useState(document.body.scrollHeight > window.innerHeight)
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(() => {
+            setBodyScrollable(document.body.scrollHeight > window.innerHeight)
+        })
+        resizeObserver.observe(document.body)
+        return () => {
+            resizeObserver.unobserve(document.body)
+        }
+    }, [])
+
+    return bodyScrollable
+}
+
+export default useBodyScrollable;
