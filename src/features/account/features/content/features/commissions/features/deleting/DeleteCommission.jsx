998f32a7b9ed8994e30861968caaec036a15df95
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-09 15:50:44 +0100

    delete feature for commission added

---
+++ src/features/account/features/content/features/commissions/features/deleting/DeleteCommission.jsx
@@ -0,0 +1,40 @@
+import TrashCanSVG from "../../../../../../../../shared/assets/media/svg/TrashCanSVG/TrashCanSVG";
+import {useState} from "react";
+import YesOrNo from "../../../../../../../../shared/component/YesOrNo/YesOrNo";
+import {ServerCommunicator} from "../../../../../../../../shared/services/ServerCommunicator";
+import {useOutletContext} from "react-router-dom";
+
+const DeleteCommission = ({id}) => {
+    const [userDetails, setUserDetails] = useOutletContext();
+    const [showQuestion, setShowQuestion] = useState(false);
+
+    const handleDelete = () => {
+        ServerCommunicator.handleRequest('delete', `/api/commissions/${id}`).then(res => {
+            if (res.success) {
+                setUserDetails(prev => {
+                    const temp = JSON.parse(JSON.stringify(prev));
+                    const newCommissions = temp.Client.AuthorCommissions.filter(com => com.id !== id);
+                    temp.Client.AuthorCommissions = [...newCommissions];
+                    console.log(temp)
+                    return temp;
+                })
+            }
+        }).finally(() => {
+            setShowQuestion(false);
+        })
+    }
+
+    return (
+        <>
+            <TrashCanSVG className={'trash-can'}
+                         onClick={() => setShowQuestion(true)}
+                         title="Usuń to zamówienie"/>
+            {showQuestion &&
+                <YesOrNo question={'Na pewno chcesz usunąć to zlecenie?'}
+                         cancelFn={() => setShowQuestion(false)}
+                         approveFn={handleDelete}/>}
+        </>
+    )
+}
+
+export default DeleteCommission;
