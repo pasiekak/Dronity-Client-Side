41cc96b646573b36493f974613abb0647e46f275
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-09 15:49:24 +0100

    new account navigation path

--- src/features/account/features/navigation/components/ClientAccountNavigation.jsx
+++ src/features/account/features/navigation/components/ClientAccountNavigation.jsx
@@ -1,22 +1,26 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const ClientAccountNavigation = ({ selected }) => {
-  return (
-    <nav className="client-account-navigation navigation">
-      <Link to="/account/settings" className={selected === 1 ? "selected" : ""}>
-        Ustawienia konta
-      </Link>
-      <Link to="/account/personal" className={selected === 2 ? "selected" : ""}>
-        Ustawienia personalne
-      </Link>
-      <Link
-        to="/account/commissions"
-        className={selected === 3 ? "selected" : ""}
-      >
-        Twoje zlecenia
-      </Link>
-    </nav>
-  );
+const ClientAccountNavigation = ({selected}) => {
+    return (
+        <nav className="client-account-navigation navigation">
+            <Link to="/account/settings"
+                  className={selected === 1 ? "selected" : ""}>
+                Ustawienia konta
+            </Link>
+            <Link to="/account/personal"
+                  className={selected === 2 ? "selected" : ""}>
+                Ustawienia personalne
+            </Link>
+            <Link to="/account/commissions"
+                  className={selected === 3 ? "selected" : ""}>
+                Twoje zlecenia
+            </Link>
+            <Link to="/account/add_commission"
+                  className={selected === 4 ? "selected" : ""}>
+                Dodaj zlecenie
+            </Link>
+        </nav>
+    );
 };
 
 export default ClientAccountNavigation;
