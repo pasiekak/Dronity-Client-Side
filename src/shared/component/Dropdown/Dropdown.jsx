32d7df37a2b20f0d5314807be37d0af3df314434
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-09 15:48:20 +0100

    typo fix

--- src/shared/component/Dropdown/Dropdown.jsx
+++ src/shared/component/Dropdown/Dropdown.jsx
@@ -1,38 +1,37 @@
-import { useState } from "react";
+import {useState} from "react";
 
 import "./dropdown.css";
-import caret from "../../assets/media/svg/CaretSVG/caret-down-fill.svg";
-import { ReactSVG } from "react-svg";
-import CaretSVG from "../../assets/media/svg/CaretSVG/CaretSVG";
-const Dropdown = ({ trigger, itemList }) => {
-  const [open, setOpen] = useState(false);
+import CaretDownSVG from "../../assets/media/svg/CaretSVG/down/CaretDownSVG";
 
-  return (
-    <div className="dropdown">
-      <div
-        className="trigger animated-hover"
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        <span>{trigger}</span>
-        <CaretSVG classname={`caret${open ? " flip" : ""}`} />
-      </div>
-      <ul className={`menu${open ? " open" : ""}`}>
-        <div className="menu-wrapper">
-          {itemList.map((item, index) => (
-            <li key={index}>
-              <button
-                onClick={() => {
-                  item.handleClick();
-                  setOpen(false);
-                }}
-              >
-                <span className="animated-hover">{item.text}</span>
-              </button>
-            </li>
-          ))}
+const Dropdown = ({trigger, itemList}) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className="dropdown">
+            <div
+                className="trigger animated-hover"
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                <span>{trigger}</span>
+                <CaretDownSVG className={`caret${open ? " flip" : ""}`}/>
+            </div>
+            <ul className={`menu${open ? " open" : ""}`}>
+                <div className="menu-wrapper">
+                    {itemList.map((item, index) => (
+                        <li key={index}>
+                            <button
+                                onClick={() => {
+                                    item.handleClick();
+                                    setOpen(false);
+                                }}
+                            >
+                                <span className="animated-hover">{item.text}</span>
+                            </button>
+                        </li>
+                    ))}
+                </div>
+            </ul>
         </div>
-      </ul>
-    </div>
-  );
+    );
 };
 export default Dropdown;
