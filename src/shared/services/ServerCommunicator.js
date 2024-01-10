d03cd6f9992697016b8971068eda5d33d71a6c6d
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-10 14:28:40 +0100

    abandonment of code handling. Transfer messages to server

--- src/shared/services/ServerCommunicator.js
+++ src/shared/services/ServerCommunicator.js
@@ -2,14 +2,12 @@ import axios from "axios";
 
 export const ServerCommunicator = {
     handleRequest: async (method, url, data) => {
-        let response = {
-            success: true,
-        };
+        let response;
         try {
             let res = await axios({
                 method, url, data
             })
-            response.data = res.data;
+            response = res.data
         } catch (err) {
             response = ServerCommunicator.handleError(err);
         }
@@ -17,16 +15,14 @@ export const ServerCommunicator = {
     },
 
     handleFilesRequest: async (url, formData) => {
-        let response = {
-            success: true,
-        };
+        let response;
         try {
             let res = await axios.post(url, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             })
-            response.data = res.data;
+            response = res.data;
         } catch (err) {
             response = ServerCommunicator.handleError(err);
         }
@@ -34,28 +30,10 @@ export const ServerCommunicator = {
     },
 
     handleError: (err) => {
-        const status = err?.response?.status;
-        const response = {
+        let response = err?.response?.data;
+        if (err?.response?.status === 500) response = {
             success: false,
-            message: 'Wystąpił błąd'
-        };
-
-        switch (status) {
-            case 400: response.message = 'Nieprawidłowe żądanie'; break;
-            case 401: response.message = 'Brak autoryzacji'; break;
-            case 402: response.message = 'Wymagana płatność'; break;
-            case 403: response.message = 'Zabronione'; break;
-            case 404: response.message = 'Nie znaleziono'; break;
-            case 409: response.message = 'Konflikt'; break;
-            case 422: response.message = 'Takie konto już istnieje'; break;
-            case 500: response.message = 'Wewnętrzny błąd serwera'; break;
-            default:
-                if (err.code === 'ECONNABORTED') {
-                    response.message = 'Żądanie przerwane z powodu przekroczenia czasu oczekiwania';
-                } else if (err.message === 'Network Error') {
-                    response.message = 'Błąd sieciowy';
-                }
-                break;
+            message: "Błąd serwera."
         }
 
         return response;
