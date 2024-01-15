934d9231b6fc47b8c4d4014a0f9108221f9132bd
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-15 10:14:39 +0100

    Changed used loader component

--- src/features/overlay/Overlay.jsx
+++ src/features/overlay/Overlay.jsx
@@ -1,21 +1,22 @@
 import "./styles/styles.css";
-import { useContext } from "react";
-import { OverlayContext } from "./context/OverlayContext";
+import {useContext} from "react";
+import {OverlayContext} from "./context/OverlayContext";
+import LoaderSVG from "../../shared/assets/media/svg/LoaderSVG/LoaderSVG";
 
 const Overlay = () => {
-  const { type, message } = useContext(OverlayContext);
+    const {type, message} = useContext(OverlayContext);
 
-  return (
-    <div className="overlay">
-      {type === "loading" && <div className="loading-spinner"></div>}
-      {type === "message" && (
-        <div className="message-container">
-          <h1>{message}</h1>
-          <div className="loading-spinner"></div>
+    return (
+        <div className="overlay">
+            {type === "loading" && <LoaderSVG height="128px"/>}
+            {type === "message" && (
+                <div className="message-container">
+                    <h1>{message}</h1>
+                    <LoaderSVG height="128px"/>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Overlay;
