91f1a0a5d55ebf6fbefb823997e02ff39f24842b
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-01-15 10:12:37 +0100

    Added accessRoles parameters, which accepts list of string roles that can access route

--- src/features/routing/components/ProtectedRoute.jsx
+++ src/features/routing/components/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
-import { Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../../account/context/UserContext";
+import {Navigate} from "react-router-dom";
+import {useContext} from "react";
+import {UserContext} from "../../account/context/UserContext";
 
-const ProtectedRoute = ({ redirectPath = "/", children }) => {
-  const { user } = useContext(UserContext);
-
-  if (!user) {
-    return <Navigate to={redirectPath} replace />;
-  }
-  return children;
+const ProtectedRoute = ({redirectPath = "/", children, accessRoles}) => {
+    const {user} = useContext(UserContext);
+    const access = accessRoles.includes(user?.Role?.name)
+    if (!user || !access) {
+        return <Navigate to={redirectPath} replace/>;
+    }
+    return children;
 };
 
 export default ProtectedRoute;
